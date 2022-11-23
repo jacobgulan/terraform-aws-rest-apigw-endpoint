1833d0ad489ee0917926c2284b64ade9b11d3a39
jacobgulan <jacobpgulan@gmail.com>
2022-11-23 15:37:05 -0500

    create base framework

--- main.tf
+++ main.tf
@@ -2,22 +2,81 @@
 # REST API Gateway Endpoint
 ##############################################################################
 
+resource "aws_api_gateway_resource" "resource" {
+  count       = var.create_api_gateway_resource ? 1 : 0
+  rest_api_id = var.rest_api_ids.id
+  parent_id   = local.parent_id
+  path_part   = var.path_part
+}
 
 ##############################################################################
 # Method Request
 ##############################################################################
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id = var.rest_api_ids.id
+}
+
+resource "aws_api_gateway_method" "options_method" {
+  count = var.create_options_method ? 1 : 0
+}
+
 
 
 ##############################################################################
 # Integration Request
 ##############################################################################
 
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = var.rest_api_ids.id
+
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  count = var.create_options_method ? 1 : 0
+}
+
 
 ##############################################################################
 # Integration Response
 ##############################################################################
 
+resource "aws_api_gateway_integration_response" "integration_response_success" {
+  rest_api_id = var.rest_api_ids.id
+
+  depends_on = [
+    aws_api_gateway_integration.integration,
+    aws_api_gateway_integration_response.integration_response,
+    aws_api_gateway_method_response.method_response_success
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = var.rest_api_ids.id
+  depends_on = [
+    aws_api_gateway_integration.integration,
+    aws_api_gateway_method_response.method_response
+  ]
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  count      = var.create_options_method ? 1 : 0
+  depends_on = [aws_api_gateway_integration.options_integration]
+}
+
 
 ##############################################################################
 # Method Response
 ##############################################################################
+
+resource "aws_api_gateway_method_response" "method_response_success" {
+  rest_api_id = var.rest_api_ids.id
+}
+
+resource "aws_api_gateway_method_response" "method_response" {
+  rest_api_id = var.rest_api_ids.id
+}
+
+resource "aws_api_gateway_method_response" "options_method_response" {
+  count = var.create_options_method ? 1 : 0
+}
--- variables.tf
+++ variables.tf
@@ -2,7 +2,68 @@
 # Required Variables
 ##############################################################################
 
+# ----------------------------------------------------------------------------
+# Base Configuration Variables
+# ----------------------------------------------------------------------------
+
+variable "rest_api_ids" {
+  type = object({
+    id               = string
+    root_resource_id = string
+  })
+  description = "REST API resource"
+}
+
+variable "http_method" {
+  description = "HTTP method. GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS"
+  type        = string
+}
+
+variable "path_part" {
+  description = "Path part of the resource. For example, 'users' in /users"
+  type        = string
+}
+
 
 ##############################################################################
 # Optional Variables
 ##############################################################################
+
+# ----------------------------------------------------------------------------
+# Base Configuration Variables
+# ----------------------------------------------------------------------------
+
+variable "create_api_gateway_resource" {
+  description = "Whether to create the API Gateway resource"
+  type        = bool
+  default     = true
+}
+
+variable "parent_id" {
+  description = "The ID of the parent resource"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources"
+  default     = {}
+}
+
+variable "lambda_status_codes" {
+  type        = list(number)
+  description = "List of status codes to map to lambda integration. 200 provided by default."
+  default     = [400, 401, 403, 404, 405, 500]
+}
+
+
+# ----------------------------------------------------------------------------
+# Options Method
+# ----------------------------------------------------------------------------
+
+variable "create_options_method" {
+  description = "Whether to create the OPTIONS method"
+  type        = bool
+  default     = true
+}
