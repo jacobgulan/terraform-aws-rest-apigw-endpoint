f608641e1f68c9e252f6cb94b0299593f39f64d8
jacobgulan <jacobpgulan@gmail.com>
2022-11-26 19:20:27 -0500

    update resource reference

--- main.tf
+++ main.tf
@@ -140,7 +140,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = var.rest_api_ids.id
   resource_id = local.resource_id
   http_method = aws_api_gateway_method.options_method[0].http_method
-  status_code = aws_api_gateway_method.options_method_response[0].status_code
+  status_code = aws_api_gateway_method_response.options_method_response[0].status_code
 
   # OPTIONAL
   response_parameters = {
