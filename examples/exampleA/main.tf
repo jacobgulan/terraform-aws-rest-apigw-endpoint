17ae59bb8657a99a0beba5b06711c54cfef5c7c3
jacobgulan <jacobpgulan@gmail.com>
2022-11-27 14:19:35 -0500

    add example

---
+++ examples/exampleA/main.tf
@@ -0,0 +1,27 @@
+##############################################################################
+# Example
+##############################################################################
+
+module "apigw_endpoint" {
+  source  = "app.terraform.io/rest-apigw-endpoint"
+  version = "0.1.0"
+
+  rest_api_ids = {
+    id               = aws_api_gateway_rest_api.api.id
+    root_resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  }
+  http_method = "PUT"
+  path_part   = "hello-world"
+  uri         = aws_lambda_function.prayer_request.invoke_arn
+
+  allow_origin         = var.env == "dev" ? "*" : "https://mydomain.com"
+  request_validator_id = aws_api_gateway_request_validator.api.id
+  integration_request_templates = {
+    "application/json" = <<-EOT
+      {
+        "body" : $input.json('$'),
+        "method" : "$context.httpMethod"
+      }
+    EOT
+  }
+}
